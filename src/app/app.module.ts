e78f7365632d50ee864a5293409056d7ecb5e4b8
Gokul <gokulchandan18@gmail.com>
2022-01-01 12:25:11 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserData } from './user-data.service';
+
+import { from } from 'rxjs';
 import { HomeComponent } from './home/home.component';
 import { GetUserComponent } from './get-user/get-user.component';
-import { DeleteUserComponent } from './delete-user/delete-user.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { CreateUserComponent } from './create-user/create-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     GetUserComponent,
-    DeleteUserComponent,
-    CreateUserComponent
+    CreateUserComponent,
+    DeleteUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, InMemoryWebApiModule.forRoot(UserData), HttpClientModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
