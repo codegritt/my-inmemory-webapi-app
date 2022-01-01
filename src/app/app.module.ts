86f188bf3d50456049cc5be6e5edb7d32932a553
Gokul <gokulchandan18@gmail.com>
2022-01-01 10:04:38 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GetUserComponent } from './get-user/get-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    GetUserComponent,
+    DeleteUserComponent,
+    CreateUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
